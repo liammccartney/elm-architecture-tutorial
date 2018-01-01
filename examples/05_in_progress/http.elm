552924b86129f557dac45e7312be83fed4523828
Liam McCartney <liam.mccartney@gmail.com>
2018-01-01 16:44:26 -0500

    [exo5] Implement Bare Minimum

---
+++ examples/05_in_progress/http.elm
@@ -0,0 +1,48 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+main =
+    Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- MODEL
+type alias Model =
+    { topic : String
+    , gifUrl : String
+    }
+
+
+init : (Model, Cmd Msg)
+init =
+    (Model "cats" "waiting.gif", Cmd.none)
+
+
+-- UPDATE
+type Msg
+    = MorePlease
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        MorePlease ->
+            (model, Cmd.none)
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [text model.topic]
+        , img [src model.gifUrl] []
+        , button [onClick MorePlease] [text "More Please!"]
+        ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
