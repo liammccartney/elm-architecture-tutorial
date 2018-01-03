a3386e226d524cadfe257d15f3848e845cbcd5b0
Liam McCartney <liam.mccartney@gmail.com>
2018-01-03 07:38:09 -0500

    [ex6] Add pause/resume button

--- examples/06_in_progress/clock.elm
+++ examples/06_in_progress/clock.elm
@@ -1,4 +1,5 @@
 import Html exposing (Html)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -14,28 +15,39 @@ main =
 
 
 -- MODEL
-type alias Model = Time
+type alias Model = 
+    { time: Time
+    , paused: Bool
+    }
 
 init : (Model, Cmd Msg)
 init =
-    (0, Cmd.none)
+    (Model 0 False, Cmd.none)
 
 
 -- UPDATE
-type Msg =
-    Tick Time
+type Msg
+    = Tick Time
+    | Pause
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Tick newTime ->
-            (newTime, Cmd.none)
+            (Model newTime False, Cmd.none)
+
+        Pause ->
+            ({ model | paused = not model.paused }, Cmd.none )
 
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    case model.paused of
+        True ->
+            Sub.none
+        False ->
+            Time.every second Tick
 
 
 
@@ -44,13 +56,27 @@ view : Model -> Html Msg
 view model =
     let
         angle =
-            turns (Time.inMinutes model)
+            turns (Time.inMinutes model.time)
         handX =
             toString (50 + 40 * cos angle)
         handY =
             toString (50 + 40 * sin angle)
     in
-        svg [ viewBox "0 0 100 100", width "300px"]
-        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-        , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        Html.div []
+        [ svg [ viewBox "0 0 100 100", width "300px"]
+          [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+          , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+          ]
+          , viewToggleButton model
         ]
+
+viewToggleButton : Model -> Html Msg
+viewToggleButton model =
+    let
+        toggleText = 
+            if model.paused then
+                "Resume"
+            else
+                "Pause"
+    in
+        Html.button [ onClick Pause ] [ Html.text toggleText ]
